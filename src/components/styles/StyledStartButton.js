a567eba876f502e05ccb59c0797d4b27c4f874ca
hagerhussein <hagarelrashidy@hotmail.com>
2019-08-16 04:56:28 +0200

    styling with styled components

---
+++ src/components/styles/StyledStartButton.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledStartButton = styled.div`
+box-sizing: border-box;
+margin: 0 0 20px 0;
+border: none;
+padding: 20px;
+min-height: 30px;
+width: 100%;
+border-radius: 20px;
+color: white;
+background: #333;
+font-family:  Press Start 2P, Arial, sans-serif;
+font-size: 1.2rem;
+outline: none;
+cursor: pointer;
+`
+
